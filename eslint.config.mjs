adcccec606dd8e59e2fb5a46aabe0f9325bb44c5
mohamedammar2729 <mo.bas.ammar@el-eng.menofia.edu.eg>
2025-03-11 18:15:55 +0200

    update eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -9,6 +9,14 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
-const eslintConfig = [...compat.extends("next/core-web-vitals")];
+const eslintConfig = [
+  ...compat.extends("next/core-web-vitals"),
+  {
+    parser: "@babel/eslint-parser",
+    parserOptions: {
+      requireConfigFile: false,
+    },
+  },
+];
 
 export default eslintConfig;
